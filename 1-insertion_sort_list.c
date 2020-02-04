81880ed13ac3f52ffeb835e0f843f2fa01c73c1b
KevCasHSC <castrok.c11@hotmail.com>
2020-02-04 14:27:54 +0000

    last upload

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -4,9 +4,9 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *cur,	*prev = NULL;
+	listint_t *cur,	*back = NULL;
 
-	if (list == NULL || *list == NULL || (*list)->next == NULL)
+	if (list == NULL || (*list) == NULL || ((*list)->next) == NULL)
 		return;
 
 	cur = *list;
@@ -15,22 +15,21 @@ void insertion_sort_list(listint_t **list)
 	{
 		while (cur->prev != NULL && cur->n < cur->prev->n)
 		{
-			prev = cur->prev;
-			prev->next = cur->next;
-			cur->prev = prev->prev;
-			prev->prev = cur;
-			cur->next = prev;
+			back = cur->prev;
+			back->next = cur->next;
+			cur->prev = back->prev;
+			back->prev = cur;
+			cur->next = back;
 
-			if(prev->next != NULL)
-				prev->next->prev = prev;
+			if (back->next != NULL)
+				back->next->prev = back;
 
 			if (cur->prev != NULL)
-				cur->next->prev = cur;
+				cur->prev->next = cur;
 
-			print_list(*list);
 			if (cur->prev == NULL)
 				*list = cur;
-			/*print_list(*list);*/
+			print_list(*list);
 		}
 		cur = cur->next;
 	}
